9caae86743bb679cb51521e998fb5c08bea7ff27
takehironegishi <takehiro.n61@gmail.com>
2021-11-28 15:08:56 +0900

    core: font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/main.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
