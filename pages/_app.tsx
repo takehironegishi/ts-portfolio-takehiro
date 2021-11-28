01dcdeba7b0d5c8a6f249e528a0627811ae1fc6e
takehironegishi <takehiro.n61@gmail.com>
2021-11-28 15:19:03 +0900

    core: scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/main.css";
+import "../styles/main.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
