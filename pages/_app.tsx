90e00979f94862edee90479125097c6edc929230
takehironegishi <takehiro.n61@gmail.com>
2021-11-28 15:01:36 +0900

    core: install reactstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
+import { Container, Button } from "reactstrap";
 import { BaseLayout } from "../components/layouts/BaseLayout";
 
 const Home: NextPage = () => (
   <BaseLayout>
+    <Container>
+      <Button color="danger">Danger</Button>
+    </Container>
     <h1>I am index page</h1>
   </BaseLayout>
 );
