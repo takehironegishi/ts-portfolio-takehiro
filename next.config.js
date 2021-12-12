314eb17958fb8da7e01e27e0b64e7be86b86640a
takehironegishi <takehiro.n61@gmail.com>
2021-12-12 16:04:17 +0900

    core: set up auth0

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const Dotenv = require("dotenv-webpack");
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.plugins.push(new Dotenv({ silent: true }));
+    return config;
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "styles/main.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
-export default MyApp;
+};
+export default App;
